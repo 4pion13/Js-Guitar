8ef782550158710005264ad16e9af515b6808d9a
Роман <i4pion@yandex.ru>
2024-09-20 12:50:45 +0300

    Визуальные изменения

--- components/Shopping/Shopping.js
+++ components/Shopping/Shopping.js
@@ -38,10 +38,10 @@ class Shopping {
             purchases.forEach(element => {   
                 const purchasesName = (CATALOG.find(item => item.id === element));
                 htmlShoppingCard += `
-                    <div class="shopping-container__item">
+                    <div class="shopping-container__item" style="margin-bottom: 10px;">
                         <div>
-                            <span class="products-element__name">${purchasesName.name}</span>
-                            <span class="products-element__price" style="margin: 0 auto">${purchasesName.price.toLocaleString()} ₽</span>
+                            <span class="products-element__name" style="margin-bottom: 0px; margin-top: 5px">${purchasesName.name}</span>
+                            <span class="products-element__price" style="margin: 0 auto; margin-bottom: 5px">${purchasesName.price.toLocaleString()} ₽</span>
                         </div>
                         <button class="shopping-container__btn" onclick = "shopping.deleteFromShoppingCard('${purchasesName.id}')"><i class="fa-solid fa-trash"></i></button>
                     </div>
@@ -52,7 +52,9 @@ class Shopping {
 
         const html = `
             <div id="shopping-card" class="shopping-container${className}">
-                <button onclick = "shopping.openShoppingCard()"><i class="fa-solid fa-xmark"></i></button>\
+                <div style = "display:flex; justify-content:end; padding: 5px;">
+                    <button class = "close-btn" onclick = "shopping.openShoppingCard()"><i class="fa-solid fa-xmark fa-lg"></i></button>\
+                </div>
                 ${htmlShoppingCard}
             </div>
         `;
